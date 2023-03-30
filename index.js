d63869f6dd67ab0265d5db2f3869666422efc89a
Yuti Thakkar <thakkaryuti@gmail.com>
2023-03-30 13:09:05 -0400

    adds ar lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+
+let random = Math.ceil(Math.random() * 100);
+
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+
+let numbers = [5, 10, 20, 15, 8];
+let max = Math.max(...numbers);
